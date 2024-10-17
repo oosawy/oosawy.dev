a755cbc766888bba6f07a1c668b7e419511beb00
Yuta Osawa <yutaosawa81@gmail.com>
2024-10-18 00:20:47 +0900

    add color transition to link icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import { preload } from 'react-dom'
 import { ProfileCard } from '~/components/profile-card'
 
 export default function Home() {
+  preload('/avatar.jpg', { as: 'image' })
+
   return <ProfileCard />
 }
