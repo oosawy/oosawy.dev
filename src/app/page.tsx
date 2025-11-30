1d23d1e61a9dbf8429474a01afdc7125e4747a85
Yuta Osawa <yutaosawa81@gmail.com>
2025-11-30 19:59:27 +0900

    add a work card: react warp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,11 @@ import ContributionsCard from '~/components/contributions-card'
 import { HourglassNotDoneIcon, HundredPointsIcon } from '~/components/icons'
 import { ProfileCard } from '~/components/profile-card'
 import { HolaCard } from '~/components/work-cards/hola-card'
-import { VectrixCard } from '~/components/work-cards/vectrix'
+import { VectrixCard } from '~/components/work-cards/vectrix-card'
+import { WarpCard } from '~/components/work-cards/warp-card'
 
 export default function Home() {
-  preload("/avatar.jpg")
+  preload('/avatar.jpg')
 
   return (
     <main className="min-h-screen bg-secondary flex items-center justify-center p-6 md:p-8">
@@ -25,7 +26,7 @@ export default function Home() {
           />
         </div>
 
-        <div className='row-span-1 col-span-1 md:row-start-3 lg:col-span-2 lg:row-start-2 lg:col-start-1'>
+        <div className="row-span-1 col-span-1 md:row-start-3 lg:col-span-2 lg:row-start-2 lg:col-start-1">
           <ZennArticleCard
             emoji={<HundredPointsIcon />}
             title={`なぜ "use client" ディレクティブは優れた API なのか`}
@@ -38,8 +39,12 @@ export default function Home() {
           <HolaCard />
         </div>
 
-        <div className="hidden lg:block col-span-1 row-span-1 lg:col-span-1 lg:row-start-3 lg:col-start-3">
+        <div className="col-span-1 row-span-1 md:row-start-4 lg:col-span-1 lg:row-start-3 lg:col-start-3">
           <VectrixCard />
+        </div>
+
+        <div className="col-span-1 row-span-1 md:row-start-4 lg:col-span-1 lg:row-start-3 lg:col-start-6">
+          <WarpCard />
         </div>
 
         <div className="col-span-2 row-span-2 sm:col-span-1 md:col-span-2 md:col-start-3 lg:row-start-1 lg:col-start-5">
---
+++ src/components/work-cards/warp-card.tsx
@@ -0,0 +1,15 @@
+export function WarpCard() {
+  return (
+    <div className="w-full grid place-items-center bg-zinc-800 rounded-lg shadow-sm hover:shadow-lg transition-shadow">
+      <div className="w-full aspect-4/3 grid place-items-center @container-[size]">
+        <a href={'https://github.com/oosawy/react-warp'} target="_blank">
+          <code className="tracking-wide space-x-1 font-medium text-[calc(16cqw)]">
+            <span className="text-neutral-300">{'<'}</span>
+            <span className="text-amber-400">{'Warp'}</span>
+            <span className="text-neutral-300">{'/>'}</span>
+          </code>
+        </a>
+      </div>
+    </div>
+  )
+}
