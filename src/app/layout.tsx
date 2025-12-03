b869bdd762a364646ac10d2aeef4f8850d305487
Yuta Osawa <yutaosawa81@gmail.com>
2025-12-03 18:19:35 +0900

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import { Analytics } from '@vercel/analytics/next'
+
 import { BreakpointIndicator } from '~/components/breakpoint-indicator'
+
+import './globals.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -30,6 +33,8 @@ export default function RootLayout({
         {children}
 
         <BreakpointIndicator />
+
+        <Analytics />
       </body>
     </html>
   )
